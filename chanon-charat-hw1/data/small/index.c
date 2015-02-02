25c8736359ef667d657178c9bd1318a802e1784e
Chanon-SVN <chanon@kmi.tl>
2015-02-02 22:53:38 +0700

    case sensitive

--- chanon-charat-hw1/data/small/index.c
+++ chanon-charat-hw1/data/small/index.c
@@ -1,54 +1,59 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+
+void change_alphabet(char* c);
+int is_alphabet(char c);
+
 int main(){
-FILE *ptr_file ;
-char buf[1000], subbuf[100];
-char* temp;  
-int x, i=0, j=0, c;
+    FILE *ptr_file ;
+    char buf[1000], subbuf[100];
+    char* temp;  
+    int x, i=0, j=0, c;
 
-        ptr_file =fopen("data/file1.txt", "r");
+    ptr_file =fopen("data/file1.txt", "r");
 
-        if (!ptr_file)
-            return 1;
+    if (!ptr_file)
+        return 1;
 
-            //Read file
-/*             do
-            {
-                
-               c = fgetc(ptr_file);
-                if( feof(ptr_file) )
-                 {              
-                     break ;
-                 }
-                printf("%c", c) ;
-             }while(1);
+    //Read file
+    fseek(ptr_file, 0, SEEK_END);
+    size_t  len = ftell(ptr_file);
+    fseek(ptr_file, 0, SEEK_SET);
+    temp= (char*)malloc(sizeof(char)*len);  
+    fread(temp,sizeof(char),len, ptr_file);
 
-           while (fgetc(ptr_file)!=EOF){
-            printf("%s\n",fgetc(ptr_file));
-           }
-            fclose(ptr_file);*/
+    printf("%s\n", temp);
 
-            //Readfile2
-             fseek(ptr_file, 0, SEEK_END);
-            size_t  len = ftell(ptr_file);
-            fseek(ptr_file, 0, SEEK_SET);
-            temp= (char*)malloc(sizeof(char)*len);  
-            fread(temp,sizeof(char),len, ptr_file);
 
-            printf("%s\n", temp);
-           //Substring file
-          /* while(buf[i]!='\0'||buf[i]!='.'){
-               j=0;
-               while(buf[i]!=' '||buf[i]!='.'){
-                subbuf[j] = buf[i];
-                i++;j++;
-               }
-               subbuf[j]='\0';
-               i++;
-               printf("%s\n",subbuf);
-           }*/
-            free(temp);
-            fclose(ptr_file);
-        return  0;
+    //Substring file
+    for(i=0;i<=len;i++){
+        j=0;
+
+        while(is_alphabet(temp[i])){
+            change_alphabet(&temp[i]);
+            subbuf[j] = temp[i];
+            i++;
+            j++;
+        }
+
+        subbuf[j]='\0';
+        if(subbuf[0]!='\0'){         
+            printf("%s\n",subbuf);
+        }
+    }
+    free(temp);
+    fclose(ptr_file);
+    return  0;
+}
+
+int is_alphabet(char c){
+
+    return ((c >= 65 && c <= 90)||( c >= 97 && c <= 122));
+}
+
+void change_alphabet(char* c){
+    if(*c >= 65 &&  *c <= 90){
+        *c += 32;
+    }
 }
