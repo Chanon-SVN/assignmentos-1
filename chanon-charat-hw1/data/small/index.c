6da5a97dba201cc45afd400d6cb159dba99f9181
Chanon-SVN <chanon@kmi.tl>
2015-02-06 00:05:58 +0700

    hash with g_ array part1

--- chanon-charat-hw1/data/small/index.c
+++ chanon-charat-hw1/data/small/index.c
@@ -2,9 +2,12 @@
 #include<string.h>
 #include<stdlib.h>
 #include<glib.h>
+#include<dirent.h>
 
 void change_alphabet(char* c);
 int is_alphabet(char c);
+int listdir(char *dir); 
+int size_array(gpointer *includes[]);
 
 int main(){
     FILE *ptr_file ;
@@ -12,7 +15,27 @@ int main(){
     char* temp;  
     int x, i=0, j=0, c, countterm=0;
 
-    ptr_file =fopen("data/file1.txt", "r");
+
+     //Create hash
+      GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
+    //Directory Manipulation
+    int file_count = 0;
+    DIR * dirp;
+    struct dirent * entry;
+    int filenum = listdir("data");
+
+
+ //   printf("File count = %d\n", filenum);
+    int filenumber = 1;
+    for(filenumber = 1; filenumber<=10;filenumber++){
+  
+    gchar path[100] = "data/file";
+        char strtemp[10];
+    snprintf(strtemp, 10,"%d", filenumber);
+    strcat(path, strtemp);
+    strcat(path, ".txt");
+    printf("File = %s\n", path);
+    ptr_file =fopen(path, "r");
 
     if (!ptr_file)
         return 1;
@@ -21,19 +44,17 @@ int main(){
     fseek(ptr_file, 0, SEEK_END);
     size_t  len = ftell(ptr_file);
     fseek(ptr_file, 0, SEEK_SET);
-    temp= (char*)malloc(sizeof(char)*len);  
+    temp= (char*)malloc(sizeof(char)*(len+1));  
     fread(temp,sizeof(char),len, ptr_file);
 
-    printf("%s\n", temp);
+   // printf("%s\n", temp);
 
 
-    //Create hash
-    GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
-    //Substring file
-    for(i=0;i<=len;i++){
-        j=0;
+   //Substring file
+     for(i=0;i<=len;i++){
+       j=0;
 
-        while(is_alphabet(temp[i])){
+       while(is_alphabet(temp[i])){
             change_alphabet(&temp[i]);
             subbuf[j] = temp[i];
             i++;
@@ -42,14 +63,30 @@ int main(){
 
         subbuf[j]='\0';
         if(subbuf[0]!='\0'){         
-            printf("%s\n",subbuf);
+     //       printf("%s\n",subbuf);
             //Keep in hash table
             gchar* key = g_strdup(subbuf);
-            g_hash_table_insert(hash, key, "DocA");
-            printf("The %p was inserted in hash table.\n", key);
+
+            GArray *garray;
+            //Check key in hash and add value.
+            if(g_hash_table_lookup(hash, key)){
+               gpointer* temp_value;
+               temp_value = g_hash_table_lookup(hash, key);
+      //         printf("array_value = %s\n", temp_value[0]);
+               g_hash_table_insert(hash, key, temp_value);
+            }
+            
+            garray = g_array_new(FALSE, FALSE, sizeof(gchar));
+          //  gchar* values[4]={};
+          //  values[0] = path;
+            g_array_append_val(garray, path);
+            g_hash_table_insert(hash, key, garray);
+          //  g_print("The %s was inserted in hash table.\n", g_array_index(garray, gchar, 0));
+
+    g_array_free(garray, TRUE);
         }
     }
-    printf("%d\n", g_hash_table_size(hash));
+    //printf("%d\n", g_hash_table_size(hash));
 
 
     GHashTableIter iter;
@@ -58,12 +95,14 @@ int main(){
     g_hash_table_iter_init (&iter, hash);
     while (g_hash_table_iter_next (&iter, &key, &value))
     {
-printf("key = %s, value= %s\n", key, value);
+       // gchar* teampchar = value;
+//    printf("key = %s, value= %s\n", key, teampchar[0]);
     }
-
-    g_hash_table_destroy(hash);
     free(temp);
     fclose(ptr_file);
+    }// CLOSE FOR LOOP
+
+    g_hash_table_destroy(hash);
     return  0;
 }
 
@@ -77,3 +116,34 @@ void change_alphabet(char* c){
         *c += 32;
     }
 }
+
+
+int listdir(char *dir) {
+      struct dirent *dp;
+        DIR *fd;
+        int count=0;
+
+          if ((fd = opendir(dir)) == NULL) {
+                  fprintf(stderr, "listdir: can't open %s\n", dir);
+                      return;
+                        }
+            while ((dp = readdir(fd)) != NULL) {
+                  if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
+                          continue;    /* skip self and parent */
+           //         printf("%s/%s\n", dir, dp->d_name);
+                    count++;
+                      }
+              closedir(fd);
+    return count;
+}
+
+int size_array(gpointer *includes[]) {
+        size_t count = 0;
+            while (includes[count]){
+                count++;
+            }
+    
+            return count;
+ }
+                
+
