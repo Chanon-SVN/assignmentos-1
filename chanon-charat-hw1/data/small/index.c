30b2bfce94910b35051bcfe5661fe51d50c239b0
Chanon-SVN <chanon@kmi.tl>
2015-02-14 13:49:51 +0700

    Version1 175Memory consumed

--- chanon-charat-hw1/data/small/index.c
+++ chanon-charat-hw1/data/small/index.c
@@ -19,25 +19,32 @@ gint compare_items (gpointer a, gpointer b);
 
     GHashTable* hash = g_hash_table_new(g_str_hash, g_str_equal);
     GPtrArray* garray;
-    char word[1000];
+    char word[100];
     char* temp;
     char dir[12] = "data/";
     char filename[15] = "file";
-
     int filenumber = 1;
     struct stat st;
+    char* file_name;
+    char temp_filenumber[15];
+    gchar* hash_key;
+    GPtrArray* filearray;
+    filearray = g_ptr_array_new();
+    //  word = malloc(100);
     for(filenumber=1;filenumber<=3000;filenumber++){
-        char path[30]="";
+        char path[30] = "";
         strcat(path, dir);
         strcat(path, filename);
-        char temp_filenumber[15];
-        char* file_name = temp_filenumber;
         snprintf(temp_filenumber, 10,"%d", filenumber);
+        file_name = temp_filenumber;
         strcat(path, temp_filenumber);
         strcat(path, ".txt");
-//        printf("File => %s\n", path);
+      //  printf("File => %s\n", path);
 
+                     g_ptr_array_add(filearray, strdup(file_name));
+//                     printf("%s\n", filearray->pdata[filenumber-1]);
         //OPEN FILE
+
         ptr_file =fopen(path, "r");
 
         //READFILE
@@ -54,50 +61,50 @@ gint compare_items (gpointer a, gpointer b);
 
         //	printf("%s\n", temp);
         //END READFILE
-
         int i,j;
         for(i=0;i<st.st_size;i++){
             j=0;
+                            
             while(is_alphabet(temp[i])){
                 word[j] = tolower(temp[i]);
                 i++;
                 j++;		
             }
-
             word[j]='\0';
-            if(word[0]!='\0'){
-                gchar* key = g_strdup(word);
-                //s = g_string_new(path);
-
+             if(word[0]!='\0'){
+                  hash_key = strdup(word);
+                //  printf("Spare = %s\n", hash_key);
                 //IF HASH EXIST...
-                if(g_hash_table_contains(hash, key)){
-                    garray = g_hash_table_lookup(hash, key); // Copy old value to GArray.
-                  //  g_print("Hello %s \n", s->str);
-                  //  g_print("eiei = %s\n", g_array_index(garray, GString*, 0)->str);
-                    if(g_strcmp0(file_name, garray->pdata[(garray->len)-1])){
-                    g_ptr_array_add(garray, g_strdup(file_name));
-                    g_hash_table_insert(hash,key, garray);
-                    }
-                 //   g_ptr_array_free(garray, FALSE);
-            //        if(!g_strcmp0(key,"translations"))
-           //         g_print("key = %s %s %s\n",key,garray->pdata[0],garray->pdata[1]);
-                }else{
-                    //IF HASH DOESN'T EXIST
-                    garray = g_ptr_array_new();
-                    g_ptr_array_add(garray, g_strdup(file_name));
-                    g_hash_table_insert(hash, key, garray);
-                 //   g_ptr_array_free(garray, FALSE);
-           //         g_print("key = %s %s \n",key,garray->pdata[0]);
-                }
-            }// END IF IN WORD
+                if(g_hash_table_contains(hash, hash_key)){
+                     garray = g_hash_table_lookup(hash, hash_key); // Copy old value to GArray.
+                   //  g_print("Hello %s \n", s->str);
+                   //  g_print("eiei = %s\n", g_array_index(garray, GString*, 0)->str);
+                     if(g_strcmp0(file_name, garray->pdata[(garray->len)-1])){
+                     g_ptr_array_add(garray, filearray->pdata[filenumber-1]);
+                     g_hash_table_insert(hash,hash_key, garray);
+                     }else{
+                     free(hash_key);
+                     }
+                  //   g_ptr_array_free(garray, FALSE);
+             //        if(!g_strcmp0(key,"translations"))
+            //         g_print("key = %s %s %s\n",key,garray->pdata[0],garray->pdata[1]);
+                 }else{
+                     //IF HASH DOESN'T EXIST
+                     garray = g_ptr_array_new();
+                     g_ptr_array_add(garray, filearray->pdata[filenumber-1]);
+                     g_hash_table_insert(hash, hash_key, garray);
+                  //   g_ptr_array_free(garray, FALSE);
+                  //   g_print("key = %s %s \n",hash_key,garray->pdata[0]);
+                 }
+             }// END IF IN WORD
         }// END FOR IN FILE
         free(temp);
+       // free(word);
         fclose(ptr_file);
 
     } // END FOR LOOP FILE
 
-   
-
+//g_ptr_array_free(garray, TRUE);   
 
     FILE *f = fopen("output", "w");
     if (f == NULL)
@@ -106,7 +113,6 @@ gint compare_items (gpointer a, gpointer b);
                 exit(1);
     }
 
-    
     GHashTableIter iter;
     gpointer key, value;
     g_hash_table_iter_init (&iter, hash);
@@ -115,7 +121,7 @@ gint compare_items (gpointer a, gpointer b);
     while (g_hash_table_iter_next (&iter, &key, &value))
     {
         //ADD IN Q_SORT
-        g_ptr_array_add(sort_hash, g_strdup(key));
+        g_ptr_array_add(sort_hash, key);
         
       //  garray = (GPtrArray*)value;
       //  int count_array=0;
@@ -127,24 +133,29 @@ gint compare_items (gpointer a, gpointer b);
       //  }
     }
         g_ptr_array_sort(sort_hash, (GCompareFunc)compare_items);
+
+    
         fprintf(f,"%d\n", sort_hash->len);
 
         int count_hash_array=0;
-        while(count_hash_array < sort_hash->len){
-       fprintf(f,"%s:",  sort_hash->pdata[count_hash_array]);
         int count_value=0;
-        GPtrArray* hash_value = g_hash_table_lookup(hash, sort_hash->pdata[count_hash_array]);
-        fprintf(f,"%d:", hash_value->len);
-        for(count_value=0; count_value < hash_value->len; count_value++){
-            fprintf(f,"%s" ,hash_value->pdata[count_value]);
-            if(count_value < hash_value->len-1){
-            fprintf(f,",");
-            }
-        }
-        fprintf(f,"\n");
+        GPtrArray* hash_value;
+        while(count_hash_array < sort_hash->len){
+        fprintf(f,"%s:",  sort_hash->pdata[count_hash_array]);
+        count_value=0;
+        hash_value = g_hash_table_lookup(hash, sort_hash->pdata[count_hash_array]);
+       fprintf(f,"%d:", hash_value->len);
+       for(count_value=0; count_value < hash_value->len; count_value++){
+           fprintf(f,"%s" ,hash_value->pdata[count_value]);
+           if(count_value < hash_value->len-1){
+           fprintf(f,",");
+           }
+      }
+       fprintf(f,"\n");
         count_hash_array++;
         }
-        
+     //   g_ptr_array_free(garray, TRUE);
+
     fclose(f);
 
 
